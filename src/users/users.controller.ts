27610d0eb8c1974d0ecfe640c8e71a8a7c4674db
hugolgs-dev <hugo.ligneres@orange.fr>
2024-12-19 18:37:07 +0100

    PATCH -> PUT

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,8 +5,8 @@ import {
   Body,
   Patch,
   Param,
-  Delete,
-} from '@nestjs/common';
+  Delete, Put
+} from "@nestjs/common";
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -30,7 +30,7 @@ export class UsersController {
     return this.usersService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Put(':id')
   update(@Param('id') id: number, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
