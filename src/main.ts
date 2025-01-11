9dee9060c4b9010a8aa3c2ad9a9dc547b1311500
hugolgs-dev <hugo.ligneres@orange.fr>
2025-01-11 21:52:20 +0100

    modif CORS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
+  app.enableCors({
+    origin: 'http://localhost:5173/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  })
+
   console.log('App listening on : ' + process.env.PORT);
   await app.listen(process.env.PORT ?? 3000);
 }
